b9c49a50ca0f75e1d79f714a75438f0f2ef91c36
ldelanier <ldelanier@cikaba.com>
2025-09-08 15:14:28 +0200

    try to fix

--- src/app/core/services/apiService.ts
+++ src/app/core/services/apiService.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.development';
 import {
   UserDTO, ProjectDTO, TaskDTO, TaskHistoryDTO, ProjectMembershipDTO, InvitationDTO, CreateTaskRequest, InvitationCreationDTO
 } from '../models/dtos/dto';
